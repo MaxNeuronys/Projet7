67425fc117f442de2bc043c9305909cf0e8f19da
MaxNeuronys <max@neuronys.ai>
2020-01-13 00:46:17 +0100

    firt commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+
+    render() {
+        return (
+            <h1 className="border">Header</h1>
+        )
+    }
+
+}
